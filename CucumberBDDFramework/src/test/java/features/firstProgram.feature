bda23747d90dc0ebb9e65cb8869816ab947f12f4
Shakil <shuvroahmed959@gmail.com>
2023-09-20 23:55:07 +0600

    Reusability feature file

--- CucumberBDDFramework/src/test/java/features/firstProgram.feature
+++ CucumberBDDFramework/src/test/java/features/firstProgram.feature
@@ -5,6 +5,15 @@ Feature: Appication Loging
 
 Scenario: Admin Page default login
 Given user is on netBanking landing Page 
-When user login into application
+When user login into application with "admin" and password "123456"
+Then Home Page is displayed
+And cards are displayed
+
+
+#Reusability
+
+Scenario: user Page default login
+Given user is on netBanking landing Page 
+When user login into application with "user" and password "123456"
 Then Home Page is displayed
 And cards are displayed
--- CucumberBDDFramework/src/test/java/stepDefinitions/MainSteps.java
+++ CucumberBDDFramework/src/test/java/stepDefinitions/MainSteps.java
@@ -13,10 +13,10 @@ public void user_is_on_net_banking_landing_page() {
     System.out.println("user is on netBanking landing Page");
 }
 
-@When("user login into application")
-public void user_login_into_application() {
+@When("user login into application with {string} and password {string}")
+public void user_login_into_application(String username, String password) {
     // Write code here that turns the phrase above into concrete actions
-    System.out.println("user login into application");
+    System.out.println(username+" and password is "+password);
 }
 
 @Then("Home Page is displayed")
