681d84835d9a3e3227d66fc173b3928b29595354
Shakil <shuvroahmed959@gmail.com>
2023-10-02 02:39:55 +0600

    added extentreports SparkReport

--- CucumberBDDFramework/src/test/java/pageObjectModel/OffersPage.java
+++ CucumberBDDFramework/src/test/java/pageObjectModel/OffersPage.java
@@ -4,25 +4,28 @@ import org.openqa.selenium.WebDriver;
 
 public class OffersPage {
 	public WebDriver driver;
-	public OffersPage(WebDriver driver) {
+
+	public OffersPage(WebDriver driver)
+	{
 		this.driver = driver;
 		
 	}
+	private By search = By.xpath("//input[@type='search']");
+	private By productName = By.cssSelector("tr td:nth-child(1)");
 	
-	 By search = By.xpath("//input[@type='search']");
-	 By productName = By.cssSelector("tr td:nth-child(1)");
 	
-	
-	public void searchItrem(String name) {
-		driver.findElement(search).sendKeys(name);	
-	}
-	 public void getSearchText () {
-		driver.findElement(search).getText();	
-	}
-	 public String getProductName() {
-		 return driver.findElement(productName).getText();
-		
+	public void searchItem(String name)
+	{
+		driver.findElement(search).sendKeys(name);
 	}
 	
-
+	public void getSearchText()
+	{
+		driver.findElement(search).getText();
+	}
+	
+	public String getProductName()
+	{
+		return driver.findElement(productName).getText();
+	}	
 }
--- CucumberBDDFramework/src/test/java/pageObjectModel/PageObjectManager.java
+++ CucumberBDDFramework/src/test/java/pageObjectModel/PageObjectManager.java
@@ -3,28 +3,35 @@ package pageObjectModel;
 import org.openqa.selenium.WebDriver;
 
 public class PageObjectManager {
-	public WebDriver driver;
-	public LandingPage landingPage;
-	public CheckoutPage checkoutPage;
-	public OffersPage offerPage; 
 	
- public PageObjectManager(WebDriver driver) {
-	 this.driver =driver; 
- }
- 
- public LandingPage getLandingPage() {
-	 landingPage = new  LandingPage(driver);
+	public LandingPage landingPage;
+	public OffersPage offersPage;
+	public WebDriver driver;
+	public CheckoutPage checkoutPage;
+	
+	public PageObjectManager(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+
+	
+	
+	public LandingPage getLandingPage()
+	{
+	
+	 landingPage= new LandingPage(driver);
 	 return landingPage;
-}
- public OffersPage offerPage() {
-	   offerPage = new OffersPage(driver);
-	   return offerPage;
- }
- 
- public CheckoutPage getCheckoutPage() {
-	 
-	 checkoutPage =new CheckoutPage(driver);
-	 return checkoutPage;
- }
- 
+	}
+	
+	public OffersPage OffersPage()
+	{
+		offersPage = new OffersPage(driver);
+		return offersPage;
+	}
+	
+	public CheckoutPage getCheckoutPage()
+	{
+		checkoutPage = new CheckoutPage(driver);
+		return checkoutPage;
+	}
 }
